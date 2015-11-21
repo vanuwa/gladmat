6e95fb9926edc6978ac85d016bea39b585a1b151
vanuwa <ikebal@localhost.localdomain>
2015-11-21 12:09:51 +0200

    modified styles for the layout; generate draft of pdf file with corresponding data;

---
+++ app/utils.js
@@ -0,0 +1,55 @@
+/**
+ * Created by ikebal on 21.11.15.
+ */
+var PDFDocument = require('pdfkit');
+var fs = require('fs');
+
+var utils = {
+  generatePDF: function (data, callback) {
+    var filename = data.first_name + '_' + data.last_name + '_' + new Date().getTime() + '.pdf';
+    var file_path = 'public/pdf/' + filename;
+    var doc = new PDFDocument();
+    var file = fs.createWriteStream(file_path);
+
+    doc.pipe(file);
+
+    doc
+      .fontSize(32)
+      .text( "Gladmat 2016", { align: 'center'} );
+
+    doc
+      .fontSize(20)
+      .text('Vågen Stavanger 20. – 23. juli 2016', { align: 'center' });
+
+    doc
+      .moveDown()
+      .fontSize(14)
+      .text('KONTRAKT', { align: 'center' });
+
+
+    doc
+      .moveDown()
+      .fontSize(14)
+      .text(JSON.stringify(data));
+
+    for (var key in data) {
+      if (data.hasOwnProperty(key)) {
+        console.log('data[key]', data[key]);
+        doc
+          .moveDown()
+          .fontSize(14)
+          .text(key + ': ' + data[key]);
+      }
+    }
+
+    doc.end();
+
+    callback(null, file_path);
+  }
+};
+
+module.exports = utils;
+
+
+
+/*utils.generatePDF({ first_name: 'Vasil', last_name: 'Ivandivka' }, function (err, result) {});*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var utils = require('../app/utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  console.log('[ POST ] body', req.body);
+
+  utils.generatePDF(req.body, function (error, result) {
+    if (error) {
+      console.log('[ ERROR ]', error);
+    } else {
+      console.log('[ PDF::PATH ]', result);
+      res.render('index', { title: 'Express' });
+    }
+  });
+
 });
 
 module.exports = router;
